9b120941a259a63642bb77ea40263bb8ec2ff45e
Gabriel José <gabriel.jm16@outlook.com>
2026-07-01 15:41:37 -0300

    add a big shot

--- player/bullets.lua
+++ player/bullets.lua
@@ -1,29 +1,95 @@
-local bulletSprite ---@type love.Image
+local bulletSprites = {
+  small = {
+    image = nil,
+    quad = nil
+  },
+  big = {
+    image = nil,
+    quad = nil
+  }
+}
 local shots = {}
 local shotDelay = 0
+local weapon = 2
 
-local function shot(x, y)
-  if shotDelay > 0 then
+local function smallShot(x, y)
+  if shotDelay > 0 or #shots >= 6 then
     return
   end
 
   table.insert(shots, {
-    x = x + 2,
+    x = x + 3,
     y = y + 2,
     sx = 0,
-    sy = -1.8
+    sy = -3
   })
   table.insert(shots, {
-    x = x + 10,
+    x = x + 9,
     y = y + 2,
     sx = 0,
-    sy = -1.8
+    sy = -3
   })
   shotDelay = 8
 end
 
+local function bigShot(x, y)
+  if shotDelay > 0 or #shots >= 12 then
+    return
+  end
+
+  table.insert(shots, {
+    x = x,
+    y = y + 2,
+    sx = 0,
+    sy = -3
+  })
+  table.insert(shots, {
+    x = x + 8,
+    y = y + 2,
+    sx = 0,
+    sy = -3
+  })
+  shotDelay = 8
+end
+
+local function shot(x, y)
+  if weapon == 1 then
+    return smallShot(x, y)
+  end
+
+  if weapon == 2 then
+    return bigShot(x, y)
+  end
+end
+
+local bigBulletSprPos = 0 -- Sprite Position
+local bigBulletAnimationPos = {0, 8, 16, 8}
+
+local function animateBigBullet()
+  local framePos = math.floor(T/3) % 4 + 1
+  local currentPos = bigBulletAnimationPos[framePos]
+
+  if currentPos ~= bigBulletSprPos then
+    bigBulletSprPos = currentPos
+    bulletSprites.big.quad:setViewport(
+      bigBulletSprPos,
+      0,
+      8,
+      16,
+      bulletSprites.big.image:getWidth(),
+      bulletSprites.big.image:getHeight()
+    )
+  end
+end
+
 local function load()
-  bulletSprite = love.graphics.newImage("assets/sprites/bullet.png")
+  local smallBulletSprite = love.graphics.newImage("assets/sprites/bullet.png")
+  bulletSprites.small.image = smallBulletSprite
+
+  local bigBulletSprite = love.graphics.newImage("assets/sprites/big-bullet.png")
+  local bigBulletQuad = love.graphics.newQuad(0, 0, 8, 16, bigBulletSprite)
+  bulletSprites.big.image = bigBulletSprite
+  bulletSprites.big.quad = bigBulletQuad
 end
 
 local function update()
@@ -35,15 +101,32 @@ local function update()
     b.x = b.x + b.sx
     b.y = b.y + b.sy
 
-    if b.y < -8 then
+    if b.y < -58 then
       table.remove(shots, i)
+    elseif weapon == 2 then
+      animateBigBullet()
     end
   end
 end
 
 local function draw()
   for _,b in pairs(shots) do
-    love.graphics.draw(bulletSprite, b.x, b.y)
+    if weapon == 1 then
+      love.graphics.draw(
+        bulletSprites.small.image,
+        b.x,
+        b.y
+      )
+    end
+
+    if weapon == 2 then
+      love.graphics.draw(
+        bulletSprites.big.image,
+        bulletSprites.big.quad,
+        b.x,
+        b.y
+      )
+    end
   end
 end
 
